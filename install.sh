2fa5fdd992e1b701360ce5ab56890f8a5bc26fa3
indhujayan <indhu.pj@litmus7.com>
2023-06-02 19:50:36 +0530

    updated sh script

--- install.sh
+++ install.sh
@@ -4,23 +4,53 @@ root_directory="."
 verdaccio_command="verdaccio"
 
 echo "Starting Verdaccio globally..."
-nohup $verdaccio_command > /dev/null 2>&1 &
+"$verdaccio_command" &
 
-node_version=$(node -v | cut -d'v' -f2)
+# Wait for Verdaccio to start (adjust the sleep duration as needed)
+sleep 10
 
-if [ "$node_version" -lt "19" ]; then
-  for i in $root_directory/*; do
-    if [ -d "$i" ] && [ -e "$i/package.json" ]; then
-      echo "Installing dependencies for $i"
-      cd "$i"
-      yarn install
-      if [ $? -ne 0 ]; then
-        echo "Error: Failed to install dependencies for $i"
-      else
-        echo "Starting application in $i"
-        nohup yarn start > /dev/null 2>&1 &
+node_version=$(node -v | awk -Fv '{print $2}')
+
+if [[ $node_version < "19" ]]; then
+  for dir in $root_directory/*; do
+    if [[ -d "$dir" ]]; then
+      if [[ -f "$dir/package.json" ]]; then
+        # Exclude specific folders from running the script
+        skipFolder=false
+        if [[ "$(basename "$dir")" == "apip-ts-types-main" ]]; then skipFolder=true; fi
+        if [[ "$(basename "$dir")" == "apip-ts-middleware-main" ]]; then skipFolder=true; fi
+        if [[ "$(basename "$dir")" == "uxdl-primitives-main" ]]; then skipFolder=true; fi
+
+        if [[ "$skipFolder" = false ]]; then
+          echo "Cleaning up $dir"
+          cd "$dir"
+
+          # Delete yarn.lock file if it exists
+          if [[ -f yarn.lock ]]; then
+            echo "Deleting yarn.lock"
+            rm yarn.lock
+          fi
+
+          # Delete package-lock.json file if it exists
+          if [[ -f package-lock.json ]]; then
+            echo "Deleting package-lock.json"
+            rm package-lock.json
+          fi
+
+          echo "Installing dependencies for $dir"
+          yarn install
+          if [[ $? -ne 0 ]]; then
+            echo "Error: Failed to install dependencies for $dir"
+          else
+            echo "Starting application in $dir"
+            yarn start &
+          fi
+
+          cd ..
+        else
+          echo "Skipping $(basename "$dir")"
+        fi
       fi
-      cd ..
     fi
   done
 else
