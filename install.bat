00b8568b7f745965f1adda93f7dea3ab4a1e45a6
Danush Renjith <renjith.dr@pg.com>
2023-06-02 16:32:22 +0530

    updated install .bat

--- install.bat
+++ install.bat
@@ -3,23 +3,50 @@ set root_directory="."
 set verdaccio_command=verdaccio
 
 echo Starting Verdaccio globally...
-start /B %verdaccio_command%
+start cmd /C "%verdaccio_command%"
+
+REM Wait for Verdaccio to start (adjust the timeout value as needed)
+timeout /T 10
 
 for /F "tokens=1,2 delims=v" %%a in ('node -v') do set "node_major_version=%%b"
 
 if "%node_major_version%" LSS "19" (
   for /D %%i in (%root_directory%\*) do (
     if exist "%%i\package.json" (
-      echo Installing dependencies for %%i
-      cd "%%i"
-      yarn install
-      if errorlevel 1 (
-        echo Error: Failed to install dependencies for %%i
+      REM Exclude specific folders from running the script
+      set "skipFolder="
+      if /I "%%~nxi"=="apip-ts-types-main" set skipFolder=true
+      if /I "%%~nxi"=="apip-ts-middleware-main" set skipFolder=true
+      if /I "%%~nxi"=="uxdl-primitives-main" set skipFolder=true
+      
+      if not defined skipFolder (
+        echo Cleaning up %%i
+        cd "%%i"
+        
+        REM Delete yarn.lock file if it exists
+        if exist yarn.lock (
+          echo Deleting yarn.lock
+          del yarn.lock
+        )
+        
+        REM Delete package-lock.json file if it exists
+        if exist package-lock.json (
+          echo Deleting package-lock.json
+          del package-lock.json
+        )
+        
+        echo Installing dependencies for %%i
+        yarn install
+        if errorlevel 1 (
+          echo Error: Failed to install dependencies for %%i
+        ) else (
+          echo Starting application in %%i
+          start cmd /C "yarn start"
+        )
+        cd ..
       ) else (
-        echo Starting application in %%i
-        start cmd.exe /k "yarn start"
+        echo Skipping %%~nxi
       )
-      cd ..
     )
   )
 ) else (
