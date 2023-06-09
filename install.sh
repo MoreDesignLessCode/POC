ddfedc024b13420a1ec70da0e45392d0412004c6
Danush Renjith <renjith.dr@pg.com>
2023-06-09 11:10:12 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,52 +4,44 @@ root_directory="."
 verdaccio_command="verdaccio"
 
 echo "Starting Verdaccio globally..."
-"$verdaccio_command" &
+$verdaccio_command &
 
-# Wait for Verdaccio to start (adjust the sleep duration as needed)
+# Wait for Verdaccio to start (adjust the sleep value as needed)
 sleep 10
 
-node_version=$(node -v | awk -Fv '{print $2}')
+node_version=$(node -v)
+node_major_version=${node_version#"v"}
 
-if [[ $node_version < "19" ]]; then
-  for dir in $root_directory/*; do
-    if [[ -d "$dir" ]]; then
-      if [[ -f "$dir/package.json" ]]; then
-        # Exclude specific folders from running the script
-        skipFolder=false
-        if [[ "$(basename "$dir")" == "apip-ts-types-main" ]]; then skipFolder=true; fi
-        if [[ "$(basename "$dir")" == "apip-ts-middleware-main" ]]; then skipFolder=true; fi
-        if [[ "$(basename "$dir")" == "uxdl-primitives-main" ]]; then skipFolder=true; fi
-
-        if [[ "$skipFolder" = false ]]; then
-          echo "Cleaning up $dir"
-          cd "$dir"
-
-          # Delete yarn.lock file if it exists
-          if [[ -f yarn.lock ]]; then
-            echo "Deleting yarn.lock"
-            rm yarn.lock
-          fi
-
-          # Delete package-lock.json file if it exists
-          if [[ -f package-lock.json ]]; then
-            echo "Deleting package-lock.json"
-            rm package-lock.json
-          fi
-
-          echo "Installing dependencies for $dir"
-          yarn install
-          if [[ $? -ne 0 ]]; then
-            echo "Error: Failed to install dependencies for $dir"
-          else
-            echo "Starting application in $dir"
-            yarn start &
-          fi
-
-          cd ..
+if [[ $node_major_version -lt 19 ]]; then
+  for i in $root_directory/*; do
+    if [[ -d "$i" && -f "$i/package.json" ]]; then
+      # Exclude specific folders from running the script
+      skipFolder=""
+      if [[ "$(basename "$i")" == "apip-ts-types-main" ]]; then
+        skipFolder=true
+      fi
+      if [[ "$(basename "$i")" == "apip-ts-middleware-main" ]]; then
+        skipFolder=true
+      fi
+      if [[ "$(basename "$i")" == "uxdl-primitives-main" ]]; then
+        skipFolder=true
+      fi
+      
+      if [ -z "$skipFolder" ]; then
+        echo "Cleaning up $i"
+        cd "$i" || exit 1
+        
+        echo "Installing dependencies for $i"
+        yarn install
+        if [ $? -ne 0 ]; then
+          echo "Error: Failed to install dependencies for $i"
         else
-          echo "Skipping $(basename "$dir")"
+          echo "Starting application in $i"
+          yarn start &
         fi
+        cd ..
+      else
+        echo "Skipping $(basename "$i")"
       fi
     fi
   done
